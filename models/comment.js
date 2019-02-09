c53a5fbf95c2f522531a2c6dd1613b0688bcb329
Manvi Tyagi <manvityagi770@gmail.com>
2019-02-09 14:00:47 +0530

    moels commited

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose")
+
+var CoommentSchema = new mongoose.Schema({
+    problem_id  : {
+        type: String, 
+        required: true
+    },
+
+    author_id  : {
+        type: String, 
+        required: false
+    },
+
+    comment_text     : {
+        type: String, 
+        required: true
+    },
+
+    by_representative: false
+    
+},{timestamps:true});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("Comment", CommentSchema);
--- models/user.js
+++ models/user.js
@@ -2,9 +2,36 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose")
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
+    firstname : {
+        type: String, 
+        required: true
+    },
+    
+    lastname  : {
+        type: String, 
+        required: true
+    },
+
+    password  : {
+        type: String, 
+        required: true
+    },
+
+    email     : {
+        type: String, 
+        required: true
+    },
+
+    aadhar    : {
+        type: String, 
+        required: true
+    },
+    
+    is_email_verified : false,
+    //submitted_problems :
+    //upvoted_problems :
+    
+},{timestamps:true});
 
 UserSchema.plugin(passportLocalMongoose);
 
