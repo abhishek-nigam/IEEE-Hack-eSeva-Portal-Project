ff03522c155138f82d12d3d83446eb1241e607dc
Abhishek Nigam <abhisheknigam1996@gmail.com>
2019-02-09 19:08:59 +0530

    Moved initialization to startup.js

--- app.js
+++ app.js
@@ -1,34 +1,12 @@
 require('dotenv').config();
 
 const express = require("express");
-const bodyParser = require('body-parser');
-const path = require('path');
 
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const User = require("./models/user")
-
-require('./startup')();
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }));
+require('./startup')(app);
 
-
-
-//PASSPORT CONFIGURATION
-app.use(require("express-session")({
-    secret: "yes i am the most strong willed person on the planet!",
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 const routes = require("./routes/index");
 app.use(routes);
--- startup.js
+++ startup.js
@@ -1,6 +1,37 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const expressSession = require('express-session');
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bodyParser = require('body-parser');
+const path = require('path');
 
-module.exports = () => {
+const User = require("./models/user")
+
+
+module.exports = (app) => {
+
+
+    app.set("view engine", "ejs");
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressSession({
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false
+    }));
+
+
+    /// Setup passport
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(User.serializeUser());
+    passport.deserializeUser(User.deserializeUser());
+
+
+    /// Connect to DB
 
     const dbDetails = {
         user: process.env.DB_USER || 'twenty_four_seven_admin',
