22b5f3651d60e25383f3883bd987be823fe26cc4
Abhishek Nigam <abhisheknigam1996@gmail.com>
2019-02-09 12:31:02 +0530

    Second commit

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var express = require("express");
-var app = express();
+require('dotenv').config();
+
+const express = require("express");
 
 
-var indexRoutes      = require("./routes/index")
+const app = express();
+app.set("view engine", "ejs");
 
 
-app.set("view engine","ejs");
+const routes = require("./routes/index");
+app.use(routes);
 
-app.use(indexRoutes);
 
-app.listen(3000,process.env.IP,function(){
-    console.log("hackathon started")
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, function () {
+    console.log(`[OK] server started on port ${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,8 @@
-var express    = require("express"),
-    router     = express.Router(),
-    passport   = require("passport"),
-    User       = require("../models/user");
+var router = require("express").Router();
 
-router.get("/", function(req,res){
-    res.render("landing");
-});
-
-//========AUTH ROUTES===========//
-
-//show register form
-router.get("/register", function(req, res){
-    res.render("register");
-});
-
-//handle sign up logic
-router.post("/register", function(req, res){
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user){
-        if(err){
-            req.flash("error",err.message);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            req.flash("success", "Welcome to YelpCamp " + user.username);
-            res.redirect("/campgrounds");
-        });
-    });
-});
+router.get('/', (req, res) => {
+    return res.send('Index page');
+})
 
 
 module.exports = router;
-    
